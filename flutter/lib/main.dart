5a94745d18fec90f839f74d4a9964f517edbbe65
JamesenPlay <jamesplasmics@gmail.com>
2026-03-17 19:45:32 +0300

    Update main.dart
    Second step

--- flutter/lib/main.dart
+++ flutter/lib/main.dart
@@ -293,6 +293,9 @@ void runConnectionManagerScreen() async {
     const DesktopServerPage(),
     MyTheme.currentThemeMode(),
   );
+  // Скрываем окно сразу после создания, чтобы избежать мелькания
+  await windowManager.hide();
+
   final hide = await bind.cmGetConfig(name: "hide_cm") == 'true';
   gFFI.serverModel.hideCm = hide;
   if (hide) {
@@ -301,7 +304,6 @@ void runConnectionManagerScreen() async {
     await showCmWindow(isStartup: true);
   }
   setResizable(false);
-  // Start the uni links handler and redirect links to Native, not for Flutter.
   listenUniLinks(handleByFlutter: false);
 }
 
